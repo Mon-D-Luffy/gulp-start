eab8fb8ea24e61440deba975566d2cf7ec6499a2
Umed <UmedWorkMail@gmail.com>
2021-12-25 23:14:23 +0300

    :part1

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -7,28 +7,31 @@ import gulpif from 'gulp-if';
 import rename from 'gulp-rename';
 import config from '../config';
 
-const copyImages = () => (
-  gulp.src(`${config.src.images}/**/*`)
-    .pipe(changed(config.dest.images))
-    .pipe(gulpif(config.isProd, imagemin([
-      imagemin.mozjpeg({ quality: 80 }),
-      imageminPngquant({ quality: [0.8, 0.9] }),
-      imagemin.svgo(),
-    ])))
-    .pipe(gulp.dest(config.dest.images))
-);
+const copyImages = () => gulp
+  .src(`${config.src.images}/**/*`)
+  .pipe(changed(config.dest.images))
+  .pipe(
+    gulpif(
+      config.isProd,
+      imagemin([
+        imagemin.mozjpeg({ quality: 80 }),
+        imageminPngquant({ quality: [0.8, 0.9] }),
+        imagemin.svgo(),
+      ]),
+    ),
+  )
+  .pipe(gulp.dest(config.dest.images));
 
-const convertImagesToWebp = () => (
-  gulp.src(`${config.src.images}/**/*.{jpg,png}`)
-    .pipe(changed(config.dest.images, { extension: '.webp' }))
-    .pipe(imagemin([
-      imageminWebp({ quality: 80 }),
-    ]))
-    .pipe(rename({
+const convertImagesToWebp = () => gulp
+  .src(`${config.src.images}/**/*.{jpg,png}`)
+  .pipe(changed(config.dest.images, { extension: '.webp' }))
+  .pipe(imagemin([imageminWebp({ quality: 80 })]))
+  .pipe(
+    rename({
       extname: '.webp',
-    }))
-    .pipe(gulp.dest(config.dest.images))
-);
+    }),
+  )
+  .pipe(gulp.dest(config.dest.images));
 
 export const imagesBuild = gulp.series(copyImages, convertImagesToWebp);
 
